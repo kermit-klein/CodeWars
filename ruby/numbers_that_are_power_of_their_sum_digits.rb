75b2c03caf83dbe18b99025bee9e9d951c9e5486
allezz <aerbay@gmail.com>
2020-07-31 20:32:50 +0200

    Add ruby kata solution

---
+++ ruby/numbers_that_are_power_of_their_sum_digits.rb
@@ -0,0 +1,25 @@
+# The number 81 has a special property, a certain power of the sum of its digits is equal to 81 (nine squared). Eighty one (81), is the first number in having this property (not considering numbers of one digit). The next one, is 512. Let's see both cases with the details
+
+# 8 + 1 = 9 and 92 = 81
+
+# 512 = 5 + 1 + 2 = 8 and 83 = 512
+
+# We need to make a function, power_sumDigTerm(), that receives a number n and may output the n-th term of this sequence of numbers. The cases we presented above means that
+
+# power_sumDigTerm(1) == 81
+
+# power_sumDigTerm(2) == 512
+
+# Happy coding!
+
+def power_sumDigTerm(n)
+  result = []
+  (2..100).each do |i|
+    (2..130).each do |j|
+      number = (i**j).to_s
+      number_digit_total = number.chars.map!(&:to_i).sum
+      result << i**j if number_digit_total == i
+    end
+  end
+  result.sort[n - 1]
+end
