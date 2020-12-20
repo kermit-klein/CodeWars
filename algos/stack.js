fefe3e7b24db7cc1c26a42ca993ae52d23a0eb07
allezz <aerbay@gmail.com>
2020-12-20 03:50:05 +0100

    Add remove duplicates

--- algos/stack.js
+++ algos/stack.js
@@ -18,3 +18,13 @@ class Stack {
     return this.store[this.top];
   }
 }
+
+function removeDup(arr) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!obj.hasOwnProperty(arr[i])) {
+      obj[arr[i]] = true;
+    }
+  }
+  return Object.keys(obj);
+}
